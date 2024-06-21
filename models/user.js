0a55b4175d94d4ed615649546a7601de61a971e8
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-21 09:12:55 +0530

    Implement password hashing and matching in user schema
    - Added 'salt' field for storing cryptographic salt
    - Implemented pre-save hook to hash password using SHA-256 and unique salt
    - Created static method 'matchPassword' to verify user-provided password during signin
    - Improved error handling for password matching and user not found scenarios

---
+++ models/user.js
@@ -0,0 +1,70 @@
+const {createHmac, randomBytes} = require("node:crypto");
+const {Schema, model} = require("mongoose");
+const { match } = require("node:assert");
+const { error } = require("node:console");
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImageURL: {
+        type: String,
+        default: "images/user-avatar.png",    
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    }
+}, 
+{timestamps: true}
+);
+
+userSchema.pre('save', function (next){
+    const user = this;
+
+    if(!user.isModified("password")) return ;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac('sha256', salt)
+    .update(user.password)
+    .digest('hex');
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next()
+});
+
+userSchema.static('matchPassword', async function(email, password) {
+    const user = await this.findOne({email});
+    if(!user) throw new Error('User Not Found!');
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac('sha256', salt)
+    .update(password)
+    .digest('hex');
+
+    if(hashedPassword !== userProvidedHash) throw new Error('Incorrect Password');
+
+    return user;
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
