06a50688d499900009cb74ab237a08c35a6015df
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-22 19:51:35 +0530

    Implement Blog model with Mongoose
    - Defined the Blog schema with fields: title, body, coverImageURL, and createdBy.
    - Set required validation for title and body fields.
    - Added a reference to the User model for the createdBy field to link blogs to users.
    - Included timestamps to automatically track creation and update times for each blog post.
    - Created the Blog model and exported it for use in the application.

---
+++ models/blog.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+// Define the schema
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    coverImageURL: {
+      type: String,
+      required: false,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User", // Make sure "User" matches the name of your user model
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Create the model
+const Blog = model("Blog", blogSchema); // "Blog" should be capitalized
+
+module.exports = Blog;
