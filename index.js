4e73497e66a83756ca000de9e18ba42cf955175d
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-22 19:43:07 +0530

    Add MongoDB connection, routes, and middleware setup
    - Established MongoDB connection to 'BlogBlooms' database.
    - Set up routes for user and blog functionalities.
    - Configured middleware for user authentication using cookies.
    - Implemented static file serving and error handling middleware.
    - Created a route handler for the homepage to fetch and display all blogs.
    - Server listens on port 8000.

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const Blog = require("./models/blog");
 
 const app = express();
 const PORT = 8000;
 const LINK = "http://localhost:8000";
 
 mongoose.connect('mongodb://127.0.0.1:27017/BlogBlooms')
-.then(e => console.log("MongoDB Connected Successfully"));
-
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch(err => console.log("MongoDB connection error:", err));
 
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res)=>{
-    res.render("home");
-})
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
+});
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+});
 
-
-app.listen(PORT, () => console.log(`Server Started at PORT": ${PORT}\nClick Here: ${LINK}`))
-
-
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}\nClick Here: ${LINK}`));
