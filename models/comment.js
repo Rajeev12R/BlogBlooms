48357f08ac8c14a3d62dbea30420c3f6291d3f04
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-24 23:54:06 +0530

    model of comment box

---
+++ models/comment.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+// Define the schema
+const commentSchema = new Schema(
+  {
+    content: {
+
+    }
+  },
+  {
+
+  }
+);
+
+// Create the model
+
+
+
