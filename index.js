2a155d9004f99699970332c2de74c38540ec5e43
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-25 19:50:47 +0530

    feat: initialize express app with database connection, user and blog routes, authentication middleware, and error handling

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+require("dotenv").config();
+
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
+const cookiePaser = require("cookie-parser");
+
 const Blog = require("./models/blog");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 8000;
@@ -12,25 +22,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/BlogBlooms')
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch(err => console.log("MongoDB connection error:", err));
 
-const userRoute = require("./routes/user");
-const blogRoute = require("./routes/blog");
-const { checkForAuthenticationCookie } = require("./middlewares/authentication");
-
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(checkForAuthenticationCookie("token"));
-app.use(express.static(path.resolve("./public")));
-
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
 app.get("/", async (req, res) => {
-    const allBlogs = await Blog.find({});
-    res.render("home", {
-        user: req.user,
-        blogs: allBlogs,
-    });
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
 });
 
 app.use("/user", userRoute);
