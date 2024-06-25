b1df5a9cd054e73717bc7ad5fc5900712cac2b4c
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-25 19:53:21 +0530

    eat: implement middleware to check for authentication cookie and populate user object

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,22 +1,21 @@
 const { validateToken } = require("../services/authentication");
 
 function checkForAuthenticationCookie(cookieName) {
-    return (req, res, next) => {
-        const tokenCookieValue = req.cookies[cookieName];
-        if (!tokenCookieValue) {
-            return next();
-        }
-        try {
-            const userPayload = validateToken(tokenCookieValue);
-            req.user = userPayload;
-            res.locals.user = userPayload;
-        } catch (error) {
-            console.error("Token validation error:", error);
-        }
-        next();
-    };
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+      return next();
+    }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {}
+
+    return next();
+  };
 }
 
 module.exports = {
-    checkForAuthenticationCookie,
+  checkForAuthenticationCookie,
 };
